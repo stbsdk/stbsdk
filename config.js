863a035b858a4449c932db890f6fcd8087558ef0
Stanislav Kalashnik <sk@infomir.eu>
2016-01-16 14:36:28 +0200

    add root STB config

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * Common configuration for STB gulp tasks.
+ *
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+// root STB config
+// to be extended in other gulp packages
+module.exports = {
+    default: {}
+};
