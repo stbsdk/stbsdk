9fc67f1d3815f0bf90e0397e83c17f20b04089e1
Stanislav Kalashnik <sk@infomir.eu>
2016-01-16 15:46:34 +0200

    rework init process

--- index.js
+++ index.js
@@ -5,25 +5,46 @@
 
 'use strict';
 
+var path   = require('path'),
+    extend = require('extend'),
+    tasks  = require('spa-gulp/lib/tasks');
+
+
+// merge configs
+// spa root + stb root + user
+extend(true,
+    require('spa-gulp/config').default,
+    require('./config').default,
+    require(path.join(process.cwd(), 'gulpfile.js')).default
+);
+
+// load and create gulp tasks
+tasks.register(extend(
+    true, {},
+    tasks.load('spa-gulp-'),
+    tasks.load('stb-gulp-')
+));
+
+
 // use all default SPA tasks
-var gulp = require('spa-gulp');
+//var gulp = require('spa-gulp');
 
 
-process.env.TARGET = 'stb';
-
-
-// additional tasks
-require('./tasks/less');
-require('./tasks/open');
-require('./tasks/proxy');
-require('./tasks/ssh');
-require('./tasks/watch');
-require('./tasks/weinre');
-
-
-// start all services
-gulp.task('serve', ['static', 'weinre', 'proxy', 'watch', 'repl']);
-
-
-// public
-module.exports = gulp;
+//process.env.TARGET = 'stb';
+//
+//
+//// additional tasks
+//require('./tasks/less');
+//require('./tasks/open');
+//require('./tasks/proxy');
+//require('./tasks/ssh');
+//require('./tasks/watch');
+//require('./tasks/weinre');
+//
+//
+//// start all services
+//gulp.task('serve', ['static', 'weinre', 'proxy', 'watch', 'repl']);
+//
+//
+//// public
+//module.exports = gulp;
