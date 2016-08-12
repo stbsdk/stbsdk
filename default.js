1e6dff18577e8e91512e38d4226dcabd8b4f348c
Stanislav Kalashnik <s.kalashnik@infomir.com>
2016-08-12 16:57:53 +0300

    rework sdk init procedure

---
+++ default.js
@@ -0,0 +1,51 @@
+/**
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+var app    = require('spasdk/lib/app'),
+    runner = app.runner;
+
+
+app.init({
+    //tasks: process.argv.slice(2),
+    //plugins: Object.keys(require('./package.json').optionalDependencies)
+    plugins: [
+        'spa-plugin-jade',
+        'spa-plugin-livereload',
+        'spa-plugin-static',
+        'spa-plugin-wamp',
+        'spa-plugin-webui',
+        'stb-plugin-css',
+        'stb-plugin-sass',
+        'stb-plugin-webpack'
+    ]
+});
+
+
+runner.task('build', runner.serial('jade:build', 'webpack:build', /*'sass:cache',*/ 'sass:build', 'css:build'));
+
+runner.task('watch', runner.parallel(
+    'jade:watch:develop',
+    'webpack:watch:develop',
+    'sass:watch:develop:720',
+    'css:watch:develop:720'
+    /*'sass:cache:watch:develop',
+     'sass:build:watch:develop'*/
+));
+
+runner.task('serve', runner.parallel(
+    'static:serve:develop',
+    'wamp:serve:default',
+    'webui:serve:default',
+    'livereload:watch:default'
+));
+
+runner.task('default', runner.serial('build', runner.parallel('watch', 'serve')));
+
+//runner.run('default');
+
+// public
+module.exports = app;
--- index.js
+++ index.js
@@ -7,50 +7,55 @@
 
 global.DEVELOP = true;
 
-var app    = require('spasdk/lib/app'),
-    runner = app.runner;
+// load user-defined config
+// by default - index.js in user project dir
+module.exports = require(require('spasdk/lib/app').paths.root);
 
 
-app.init({
-    //tasks: process.argv.slice(2),
-    //plugins: Object.keys(require('./package.json').optionalDependencies)
-    plugins: [
-        'spa-plugin-jade',
-        'spa-plugin-livereload',
-        'spa-plugin-static',
-        'spa-plugin-wamp',
-        'spa-plugin-webui',
-        'stb-plugin-css',
-        'stb-plugin-sass',
-        'stb-plugin-webpack'
-    ]
-});
-
-
-runner.task('build', runner.serial('jade:build', 'webpack:build', /*'sass:cache',*/ 'sass:build', 'css:build'));
-
-runner.task('watch', runner.parallel(
-    'jade:watch:develop',
-    'webpack:watch:develop',
-    'sass:watch:develop:720',
-    'css:watch:develop:720'
-    /*'sass:cache:watch:develop',
-    'sass:build:watch:develop'*/
-));
-
-runner.task('serve', runner.parallel(
-    'static:serve:develop',
-    'wamp:serve:default',
-    'webui:serve:default',
-    'livereload:watch:default'
-));
-
-runner.task('default', runner.serial('build', runner.parallel('watch', 'serve')));
-
-//runner.run('default');
-
-// public
-module.exports = app;
+// var app    = require('spasdk/lib/app'),
+//     runner = app.runner;
+//
+//
+// app.init({
+//     //tasks: process.argv.slice(2),
+//     //plugins: Object.keys(require('./package.json').optionalDependencies)
+//     plugins: [
+//         'spa-plugin-jade',
+//         'spa-plugin-livereload',
+//         'spa-plugin-static',
+//         'spa-plugin-wamp',
+//         'spa-plugin-webui',
+//         'stb-plugin-css',
+//         'stb-plugin-sass',
+//         'stb-plugin-webpack'
+//     ]
+// });
+//
+//
+// runner.task('build', runner.serial('jade:build', 'webpack:build', /*'sass:cache',*/ 'sass:build', 'css:build'));
+//
+// runner.task('watch', runner.parallel(
+//     'jade:watch:develop',
+//     'webpack:watch:develop',
+//     'sass:watch:develop:720',
+//     'css:watch:develop:720'
+//     /*'sass:cache:watch:develop',
+//     'sass:build:watch:develop'*/
+// ));
+//
+// runner.task('serve', runner.parallel(
+//     'static:serve:develop',
+//     'wamp:serve:default',
+//     'webui:serve:default',
+//     'livereload:watch:default'
+// ));
+//
+// runner.task('default', runner.serial('build', runner.parallel('watch', 'serve')));
+//
+// //runner.run('default');
+//
+// // public
+// module.exports = app;
 
 
 /*var path   = require('path'),
